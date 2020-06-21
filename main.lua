ad110256983bcf84e259e47600ba3bf617d986c5
zbouvier <zbouvier99@gmail.com>
2020-06-21 10:45:00 -0400

    Fixed sorting algorithm and changed print message

--- main.lua
+++ main.lua
@@ -2,10 +2,10 @@
 tableOfInterrupts = {
   {id = "BalanceDruid", cooldown =60, cleanName = "Balance Druid", playerName = ""},
   {id = "DAMAGERDruid", cooldown =15, cleanName = "Feral Druid", playerName = ""},
-  {id = "TANKDruid", cooldown =15, cleanName = "Guardian Druid", playerName = ""},
+  {id = "TANKDruid", cooldown =1, cleanName = "Guardian Druid", playerName = ""},
   {id = "DAMAGERHunter", cooldown =24, cleanName = "Hunter", playerName = ""},
   {id = "DAMAGERPriest", cooldown =45, cleanName = "Shadow Priest", playerName = ""},
-  {id = "DAMAGERShaman", cooldown =12, cleanName = "DPS Shaman", playerName = ""},
+  {id = "DAMAGERShaman", cooldown =2, cleanName = "DPS Shaman", playerName = ""},
   {id = "HEALERShaman", cooldown =12, cleanName = "Restoration Shaman", playerName = ""},
   {id = "DAMAGERWarrior", cooldown =15, cleanName = "DPS Warrior", playerName = ""},
   {id = "TANKWarrior", cooldown =15, cleanName = "Protection Warrior", playerName = ""},
@@ -37,70 +37,66 @@ end
 groupindex = 0;
 currentGroupInterrupts = {};
 
-
+function compare(a,b)
+  return a.cooldown < b.cooldown
+end
 
 function dungeonHandler(self, event, isInitialLogin, isReloadingUi, ...)
   inInstance, instanceType = IsInInstance()
   if(instanceType == "party") then
       name, instanceType, difficultyID, difficultyName, maxPlayers, dynamicDifficulty, isDynamic, instanceID, instanceGroupSize, LfgDungeonID = GetInstanceInfo()
       ChatFrame1:AddMessage("EDH -- You have entered: "..name, 125, 0, 125, 3)
-      if(UnitInParty("player")) then
-        priority = 1;
-        for groupindex = 1,MAX_PARTY_MEMBERS do
-          
-          local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML, combatRole = GetRaidRosterInfo(groupindex);
-          if(class) then 
-            --ChatFrame1:AddMessage(name, 125, 0, 125)
-            tableKey = combatRole..class
-            if(class == "Druid") then
-              powerType, powerTypeString = UnitPowerType("party"..groupindex);
-              --ChatFrame1:AddMessage(powerType, 125, 0, 125)
-              if (powerType == 0 and combatRole == "DAMAGER") then -- Power Type 0 is Energy, 1 is mana or DC, Astral is 8
-              else
-                tableKey = "BalanceDruid"
-              end
-              
-            end
-            --ChatFrame1:AddMessage(tableKey, 125, 0, 125)
-            --ChatFrame1:AddMessage(powerType, 125, 0, 125)
-            if(not (interruptLookupTable[tableKey].cooldown == -1)) then --Ignores Healers
-              if(priority == 1) then
-                currentGroupInterrupts[priority] = {individualName = name, cooldown = interruptLookupTable[tableKey].cooldown}
-              else
-                if(interruptLookupTable[tableKey].cooldown > currentGroupInterrupts[priority-1].cooldown) then
-                  currentGroupInterrupts[priority] = currentGroupInterrupts[priority-1]
-                  currentGroupInterrupts[priority-1] = {individualName = name, cooldown = interruptLookupTable[tableKey].cooldown}
-                else
-                  currentGroupInterrupts[priority] = {individualName = name, cooldown = interruptLookupTable[tableKey].cooldown}
-                end
-              end
-              --SendChatMessage(name..": "..interruptLookupTable[tableKey].cooldown.." seconds","PARTY" ,"COMMON" ,1);
-              priority = priority + 1;
-            end
-
-          end
-        end
-        SendChatMessage("~ EDH ~ Here is your parties' interrupt order!","PARTY" ,"COMMON" ,1);
-        for k, v in pairs(currentGroupInterrupts) do
-          if(k == 1) then
-            timing = "First"
-          elseif(k==2) then
-            timing = "Second"
-          elseif(k==3) then
-            timing = "Third"
-          elseif(k==4) then
-            timing = "Fourth"
-          else
-            timing = "Fifth"
-          end
-          if(v) then
-            SendChatMessage(timing ..":"..v.individualName,"PARTY" ,"COMMON" ,1, 3);
-          end
-        end
-      end
+      printInterrupts()
   end
 end
 
+function printInterrupts()
+  if(UnitInParty("player")) then
+    priority = 1;
+    for groupindex = 1,MAX_PARTY_MEMBERS do
+      
+      local name, rank, subgroup, level, class, fileName, zone, online, isDead, role, isML, combatRole = GetRaidRosterInfo(groupindex);
+      if(class) then 
+        --ChatFrame1:AddMessage(name, 125, 0, 125)
+        tableKey = combatRole..class
+        if(class == "Druid") then
+          powerType, powerTypeString = UnitPowerType("party"..groupindex);
+          --ChatFrame1:AddMessage(powerType, 125, 0, 125)
+          if (powerType == 8 and combatRole == "DAMAGER") then -- Power Type 0 is Energy, 1 is mana or DC, Astral is 8
+            tableKey = "BalanceDruid"
+          end
+          
+          
+        end
+        currentGroupInterrupts[priority] = {individualName = name, cooldown = interruptLookupTable[tableKey].cooldown}
+          --SendChatMessage(name..": "..interruptLookupTable[tableKey].cooldown.." seconds","PARTY" ,"COMMON" ,1);
+        priority = priority + 1;
+        --table.sort(currentGroupInterrupts, compare)
+
+
+      end
+    end
+    table.sort(currentGroupInterrupts, compare)
+    SendChatMessage("EDH:Suggested Interrupt Order","PARTY" ,"COMMON" ,1);
+    for k, v in pairs(currentGroupInterrupts) do
+      --ChatFrame1:AddMessage(k .. " " .. v.cooldown, 125, 0, 125, 3)
+      if(k == 1) then
+        timing = "First or {Diamond}"
+      elseif(k==2) then
+        timing = "Second or {Moon}"
+      elseif(k==3) then
+        timing = "Third or {Square}"
+      elseif(k==4) then
+        timing = "Fourth or {Circle}"
+      else
+        timing = "Fifth or {Star}"
+      end
+      if(v) then
+        SendChatMessage(timing ..": "..v.individualName,"PARTY" ,"COMMON" ,1, 3);
+      end
+    end
+  end
+end
 -- function inspectHandler(self, event,...)
 --   local specID = GetInspectSpecialization("party"..groupindex)
 --   if specID and specID > 0 then
@@ -113,7 +109,11 @@ local playerEnterWorld = CreateFrame("Frame");
 playerEnterWorld:RegisterEvent("PLAYER_ENTERING_WORLD");
 playerEnterWorld:SetScript("OnEvent", dungeonHandler);
 
-
+SLASH_EDH1 = "/edh"
+SLASH_EDH2 = "/dh"
+SlashCmdList["EDH"] = function(msg)
+   printInterrupts()
+end 
 -- local inspectReady = CreateFrame("FRAME");
 -- inspectReady:RegisterEvent("INSPECT_READY");
 -- inspectReady:SetScript("OnEvent", inspectHandler);
@@ -145,3 +145,5 @@ playerEnterWorld:SetScript("OnEvent", dungeonHandler);
 -- interruptLookupTable["HEALERDruid"] = -1
 -- interruptLookupTable["HEALERPriest"] = -1
 -- interruptLookupTable["HEALERMonk"] = -1
+
+
